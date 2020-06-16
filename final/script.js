ffe0c3087bc7d8076da7d059226953fa1bbcecc6
edweeniie <61220859+edweeniie@users.noreply.github.com>
2020-06-15 19:55:12 -0700

    finished final
    will try to make a few layout changed to make it look cleaner, but overall finished. Got all the requirements down.

--- final/script.js
+++ final/script.js
@@ -1,1 +1,12 @@
-// JavaScript Document
+// JavaScript Document				 					  
+//	$(document).ready(function(){
+//			$("#add").click(function(){
+//				$("small-blog").append('<div class="small-text">')
+//			})
+//		})
+
+$(document).ready(function(){
+	$('#show').click(function(){
+		$('.hiddencolumn').toggle("slide");
+	})
+})
